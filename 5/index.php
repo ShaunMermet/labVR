a15a4140e41b8d06477a22ddc4fbf05280e25ecf
ShaunMermet <shaun.mermet@gmail.com>
2017-06-12 09:13:59 +0900

    More samples + improvments

--- 5/index.php
+++ 5/index.php
@@ -8,10 +8,11 @@
   </head>
   <body>
     <a-scene>
-	    <a-assets>
-			<img id="groundTexture" src="img/floor.jpg">
-	    	<img id="skyTexture" src="img/sky.jpg">
-	    	<a-asset-item id="myPlyModel" src="soybean_tanashi_x5rgb15m_20161227_group1_densified_point_cloudGood3InvertedNorm.ply"></a-asset-item>
+	    <a-entity camera="userHeight: 1.6" universal-controls="fly: true;movementEasingY: 15;"s></a-entity>
+		<a-assets>
+			<img id="groundTexture" src="../img/floor.jpg">
+	    	<img id="skyTexture" src="../img/sky3b.jpg">
+	    	<a-asset-item id="myPlyModel" src="soybean_tanashi_x5rgb15m_20161110_group1_densified_point_cloudNew2.ply"></a-asset-item>
 	  	</a-assets>
 	  	<a-entity ply-model="src: #myPlyModel" position="0 1 0" rotation="-90 0 0" ></a-entity>
 	  	<a-cylinder id="ground" src="#groundTexture" radius="30" height="0.1"></a-cylinder>
